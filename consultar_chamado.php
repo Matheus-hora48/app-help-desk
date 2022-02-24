8702a509d3d5988532f9a4cd448a1e205083df30
Matheus Hora <matheus11hora@gmail.com>
2022-02-24 10:29:06 -0300

    mudanças

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -59,7 +59,7 @@ fclose($arquivo);
 
   <nav class="navbar navbar-dark bg-dark">
     <a class="navbar-brand" href="#">
-      <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+      <img src="/assets/img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
       App Help Desk
     </a>
     <ul class="navbar-nav">
